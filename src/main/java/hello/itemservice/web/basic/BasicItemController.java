4791f63c516e5468633812cc6f42657cd957338f
WARDKEY <tjrmsduq@naver.com>
2024-09-04 15:00:06 +0900

    상품 수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -130,6 +130,22 @@ public class BasicItemController {
 		return "basic/item";
 	}
 
+	// 상품 수정 폼 이동
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable Long itemId, Model model){
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/editForm";
+	}
+
+	// 상품 수정 처리
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+		itemRepository.update(itemId, item);
+		return "redirect:/basic/items/{itemId}";
+
+	}
+
 	/**
 	 * 테스트용 데이터 추가
 	 */
