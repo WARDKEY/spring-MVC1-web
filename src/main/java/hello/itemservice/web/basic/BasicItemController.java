581ff364b712f15b109698bac4539f5d33d8d321
WARDKEY <tjrmsduq@naver.com>
2024-09-04 14:31:05 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -52,16 +54,80 @@ public class BasicItemController {
 		return "basic/item";
 	}
 
+	// 상품 등록 폼 이동
 	@GetMapping("/add")
 	public String addForm(){
 
 		return "basic/addForm";
 	}
 
-	@PostMapping("/add")
-	public String save(){
+	// 상품 등록 처리
+	// form-data 형식
+	// @PostMapping("/add")
+	// 매개변수 이름은 html의 input에 name을 확인
+	public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model){
+		// 파라미터가 오면 item 객체 생성하여 값 담아준 다음
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
 
-		return "basic/addForm";
+		// 그 item 객체를 저장
+		itemRepository.save(item);
+
+		model.addAttribute("item", item);
+
+		return "basic/item";
+	}
+
+	// 상품 등록 처리
+	/**
+	 * @ModelAttribute("item") Item item
+	 * model.addAttribute("item", item); 자동 추가 */
+	// @PostMapping("/add")
+	// 매개변수 이름은 html의 input에 name을 확인
+	public String addItemV2(@ModelAttribute("item") Item item, Model model){
+
+		// @ModelAttribute 사용하면 item 객체 자동 생성
+
+		itemRepository.save(item);
+		// model.addAttribute("item", item);	// 자동 추가되어 생략 가능
+
+		return "basic/item";
+	}
+
+	// 상품 등록 처리
+	/**
+	 * @ModelAttribute name 생략 가능
+	 * model.addAttribute(item); 자동 추가, 생략 가능
+	 * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item */
+	// @PostMapping("/add")
+	// 매개변수 이름은 html의 input에 name을 확인
+	public String addItemV3(@ModelAttribute Item item){
+		// 이름값을 지우면 클래스명인 Item에서 첫 글자를 소문자로 바꾼 item이 이름이 된다.
+		// @ModelAttribute 사용하면 item 객체 자동 생성
+
+		itemRepository.save(item);
+		// model.addAttribute("item", item);	// 자동 추가되어 생략 가능
+
+		return "basic/item";
+	}
+
+	// 상품 등록 처리
+	/**
+	 * @ModelAttribute 자체 생략 가능
+	 * model.addAttribute(item) 자동 추가 */
+	@PostMapping("/add")
+	// 매개변수 이름은 html의 input에 name을 확인
+	public String addItemV4(Item item){
+		// @ModelAttribute를 생략하면 파라미터가 객체인 경우 자동으로 @ModelAttribute 적용
+		// 이름값을 지우면 클래스명인 Item에서 첫 글자를 소문자로 바꾼 item이 이름이 된다.
+		// @ModelAttribute 사용하면 item 객체 자동 생성
+
+		itemRepository.save(item);
+		// model.addAttribute("item", item);	// 자동 추가되어 생략 가능
+
+		return "basic/item";
 	}
 
 	/**
